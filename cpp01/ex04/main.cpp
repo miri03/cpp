807577cf737a25b55e1a15c0e72db7e9353ec807
Meryem Harit <meharit@e2r13p8.1337.ma>
2023-09-10 16:49:29 +0100

    cpp01-ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: meharit <meharit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/06 13:18:25 by meharit           #+#    #+#             */
-/*   Updated: 2023/09/09 22:01:41 by meharit          ###   ########.fr       */
+/*   Created: 2023/09/10 14:29:34 by meharit           #+#    #+#             */
+/*   Updated: 2023/09/10 16:43:52 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,60 +16,30 @@ int main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		int				len = strlen(argv[2]);
-		int				find = -len;
 		std::string		buff;
-		std::string		og;
-		std::ifstream	file;
-		std::ofstream	newfile;
+		std::ifstream	ifs;
+		std::ofstream	ofs;
+		int				len = strlen(argv[3]);
+		int				find = -len;
 		
-		file.open(argv[1], std::ifstream::in);
-		newfile.open("new_file.replace", std::ifstream::out);
-		getline(file, buff, '\0');
-		int	i = 0;
-		// if (!file.eof())
-		// {
-			std::cout << "ok"  << std::endl;
-			
-			
-			// og = buff;
-			// std::cout << std::string::npos << std::endl;
-			// find = buff.find(argv[2], find + len);
-			i = 0;
-			find = -len;
+		ifs.open(argv[1], std::ifstream::in);
+		ofs.open("new.replace", std::ofstream::out);
+		getline(ifs, buff, '\0');
+		int i = 0;
+		while (1)
+		{
 			find = buff.find(argv[2], find + len);
-			while (1)
-			{
-				std::cout << find << std::endl;
-				std::cout << "i = " << i << std::endl;
-				og.append(buff.substr(i, find - i));
-				i = find + len;
-				std::cout << "i = " << i << std::endl;
-				og.append(argv[3]);
-				find = buff.find(argv[2], find + len);
-				if (find == -1)	{
-					og.append(buff.substr(i, buff.length() - find));
-					break;
-				}
-				std::cout << find << std::endl;
-			}
-			
-			
-			std::cout << og;
-			// while (argv[3][i])
-			// {
-			// 	buff[find] = argv[3][i];
-			// 	std::cout << "s2 => " << argv[3][i] << " buff => " << buff[find] << std::endl;
-			// 	i++;
-			// 	find++;
-			// }
-			// std::cout << buff << std::endl;
-			// newfile << buff;
-		// }
-		newfile << og;
-		file.close();
-		newfile.close();
+			std::cout << find << std::endl;
+			if (find == -1 || strlen(argv[2])== 0)
+				break;
+			buff.insert(find, argv[3]);
+			buff.erase(find + len, strlen(argv[2]));
+			i++;
+		}
+		ofs << buff;
+		ifs.close();
+		ofs.close();
 	}
 	else
-		std::cout << "enter the filename and two strings, s1 and s2" << std::endl;
+		std::cout << "Enter filename and two strings, s1 and s2" << std::endl;
 }
