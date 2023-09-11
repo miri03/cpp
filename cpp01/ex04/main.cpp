779a35f9985e4e6fc61ee1c2af587ae8bb6e238b
miri03 <haritmeryem@gmail.com>
2023-09-11 17:59:41 +0100

    cpp01-ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,13 @@
 /*   By: meryemharit <meryemharit@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 14:29:34 by meharit           #+#    #+#             */
-/*   Updated: 2023/09/11 17:12:23 by meryemharit      ###   ########.fr       */
+/*   Updated: 2023/09/11 17:55:50 by meryemharit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "replace.hpp"
 
-void	open_files(std::ifstream &ifs, std::ofstream &ofs, char *filename)
+void	open_files(std::ifstream &ifs, std::ofstream &ofs, char *filename, std::string &buff)
 {
 	std::string	new_f;
 	ifs.open(filename, std::ifstream::in);
@@ -21,7 +21,13 @@ void	open_files(std::ifstream &ifs, std::ofstream &ofs, char *filename)
 		perror("original file");
 		exit (1);
 	}
-	////////////////////////////////
+	getline(ifs, buff, '\0');
+	if (!buff[0])
+	{
+		std::cout << "empty file" << std::endl;
+		exit (1);
+	}
+	////////////////////////////////	
 	new_f.append(filename);
 	new_f.append(".replace", 0, 8);
 	////////////////////////////////
@@ -43,8 +49,7 @@ int main(int argc, char **argv)
 		int				len = strlen(argv[3]);
 		int				find = -len;
 		
-		open_files(ifs, ofs, argv[1]);	
-		getline(ifs, buff, '\0');
+		open_files(ifs, ofs, argv[1], buff);
 		while (1)
 		{
 			find = buff.find(argv[2], find + len);
