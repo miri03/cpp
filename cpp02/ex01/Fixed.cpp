23728cac8cecfc4d15faadfc1853338bffd0e432
miri03 <haritmeryem@gmail.com>
2023-09-23 01:06:02 +0100

    cpp02-ex01

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: meharit <meharit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 18:04:56 by meharit           #+#    #+#             */
-/*   Updated: 2023/09/21 21:08:19 by meharit          ###   ########.fr       */
+/*   Updated: 2023/09/23 01:03:19 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,18 @@ Fixed::Fixed()
 
 Fixed::Fixed(const int nb)
 {
-	
+	std::cout << "Int constructor called" << " " << nb << std::endl;
+	value = roundf(nb * (1 << Fract_bits));
+	std::cout << "[int] value = " << value << std::endl;
 }
 
 Fixed::Fixed(const float nb)
-{}
+{
+	std::cout << "Float constructor called" << " " << nb << std::endl;
+	value = roundf(nb * (1 << Fract_bits));
+	std::cout << "value = " << value << std::endl;
+	
+}
 
 Fixed::Fixed(const Fixed& og)
 {
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meharit <meharit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:57:24 by meharit           #+#    #+#             */
-/*   Updated: 2023/09/21 18:58:07 by meharit          ###   ########.fr       */
+/*   Updated: 2023/09/23 00:17:54 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ int main( void ) {
 
 	a = Fixed( 1234.4321f );
 	
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
+	// std::cout << "a is " << a << std::endl;
+	// std::cout << "b is " << b << std::endl;
+	// std::cout << "c is " << c << std::endl;
+	// std::cout << "d is " << d << std::endl;
 	
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	
 	return 0;
 }
